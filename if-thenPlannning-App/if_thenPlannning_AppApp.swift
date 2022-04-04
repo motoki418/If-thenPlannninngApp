ab756b7d1f5a1daa62383a255f380fcf98db0520
motoki418 <snkrs418@gmail.com>
2022-04-04 20:02:24 +0900

    Initial Commit

---
+++ if-thenPlannning-App/if_thenPlannning_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  if_thenPlannning_AppApp.swift
+//  if-thenPlannning-App
+//
+//  Created by nakamura motoki on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct if_thenPlannning_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
